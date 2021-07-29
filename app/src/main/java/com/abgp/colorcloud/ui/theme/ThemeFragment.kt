53694a5d49cca774a102c0e2db3af51f5affd565
crisxchan <chrisgonzales.online@gmail.com>
2021-07-29 15:27:30 +0800

    WIP: Upgrade change theme UI

--- app/src/main/java/com/abgp/colorcloud/ui/theme/ThemeFragment.kt
+++ app/src/main/java/com/abgp/colorcloud/ui/theme/ThemeFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -39,11 +40,11 @@ class ThemeFragment : Fragment() {
         })
         bnd.rgThemes.setOnCheckedChangeListener { _, checkedId ->
             when(checkedId){
-                R.id.rbRoseanna -> changeTheme("#ffafbd", "#ffc3a0")
-                R.id.rbPurpleLove -> changeTheme("#cc2b5e", "#753a88")
-                R.id.rbMauve -> changeTheme("#42275a", "#734b6d")
-                R.id.rbSexyBlue -> changeTheme("#2193b0", "#6dd5ed")
-                R.id.rbFrost -> changeTheme("#000428", "#004e92")
+                R.id.rbRoseanna -> changeTheme("#F17B41", "#E05BA2", "#CD4BC9")
+                R.id.rbVoid -> changeTheme("#274B74", "#8233C5", "#E963FD")
+                R.id.rbMauve -> changeTheme("#C3ADC3", "#BD959F", "#EEB5B5")
+                R.id.rbGlacier -> changeTheme("#8929AD", "#436AAC", "#43B7B8")
+                R.id.rbBorealis -> changeTheme("#276174", "#33C58E", "#63FD88")
             }
         }
         return root
@@ -54,40 +55,38 @@ class ThemeFragment : Fragment() {
         _binding = null
     }
 
-    private fun changeTheme(firstColor: String, secondColor: String){
+    private fun changeTheme(firstColor: String, secondColor: String, thirdColor: String){
         val gradient1Drawable = GradientDrawable(
-            GradientDrawable.Orientation.TOP_BOTTOM,
+            GradientDrawable.Orientation.TR_BL,
             intArrayOf(
                 Color.parseColor(firstColor),
                 Color.parseColor(secondColor),
-                Color.parseColor(firstColor))
+                Color.parseColor(thirdColor))
         )
         val gradient2Drawable = GradientDrawable(
             GradientDrawable.Orientation.TOP_BOTTOM,
             intArrayOf(
                 Color.parseColor(secondColor),
-                Color.parseColor(firstColor),
+                Color.parseColor(thirdColor),
                 Color.parseColor(firstColor))
         )
         val gradient3Drawable = GradientDrawable(
             GradientDrawable.Orientation.TOP_BOTTOM,
             intArrayOf(
-                Color.parseColor(firstColor),
+                Color.parseColor(thirdColor),
                 Color.parseColor(firstColor),
                 Color.parseColor(secondColor))
         )
 
-        // mora ni syag maghimo kag gradient animation nga drawable. pero programmatically.
         val animaDrawable = AnimationDrawable()
-        animaDrawable.addFrame(gradient1Drawable, 3000)
-        animaDrawable.addFrame(gradient2Drawable, 3000)
-        animaDrawable.addFrame(gradient3Drawable, 3000)
+        animaDrawable.addFrame(gradient1Drawable, 2000)
+        animaDrawable.addFrame(gradient2Drawable, 2000)
+        animaDrawable.addFrame(gradient3Drawable, 2000)
 
+        bnd.clLayout.background = animaDrawable
 
-        // diri dapit iya iset ang animation as background
-        bnd.clLayout.background = animaDrawable              // set background sa root_layout
         animaDrawable.setEnterFadeDuration(10)
-        animaDrawable.setExitFadeDuration(3000)
+        animaDrawable.setExitFadeDuration(2000)
         animaDrawable.start()
     }
 
